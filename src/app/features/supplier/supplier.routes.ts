62b4bd5caf3ca40ec0dbff45011f60ebf9160a39
Zayhn Ahmed <ahmedzayhn@gmail.com>
2026-05-14 01:36:08 +0530

    Added /rfqs, /rfqs/:id

--- src/app/features/supplier/supplier.routes.ts
+++ src/app/features/supplier/supplier.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { SupplierDashboardPage } from './pages/supplier-dashboard.page/supplier-dashboard.page';
+import { SupplierRfqListPage } from './pages/rfq-list.page/rfq-list.page';
+import { SupplierRfqDetailPage } from './pages/rfq-detail.page/rfq-detail.page';
 
 export const routes: Routes = [
   {
     path: '',
     component: SupplierDashboardPage,
+  },
+  {
+    path: 'rfqs',
+    component: SupplierRfqListPage,
+  },
+  {
+    path: 'rfqs/:id',
+    component: SupplierRfqDetailPage,
   },
 ];
