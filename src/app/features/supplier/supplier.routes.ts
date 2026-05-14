97804880c02bae2967cfb002f611abe77cb520bd
Zayhn Ahmed <ahmedzayhn@gmail.com>
2026-05-15 01:16:34 +0530

    Added /purchase-orders, /purchase-orders/:id

--- src/app/features/supplier/supplier.routes.ts
+++ src/app/features/supplier/supplier.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { SupplierDashboardPage } from './pages/supplier-dashboard.page/supplier-dashboard.page';
 import { SupplierRfqListPage } from './pages/rfq-list.page/rfq-list.page';
 import { SupplierRfqDetailPage } from './pages/rfq-detail.page/rfq-detail.page';
+import { SupplierPoListPage } from './pages/po-list.page/po-list.page';
+import { SupplierPoDetailPage } from './pages/po-detail.page/po-detail.page';
 
 export const routes: Routes = [
   {
@@ -15,5 +17,13 @@ export const routes: Routes = [
   {
     path: 'rfqs/:id',
     component: SupplierRfqDetailPage,
+  },
+  {
+    path: 'purchase-orders',
+    component: SupplierPoListPage,
+  },
+  {
+    path: 'purchase-orders/:id',
+    component: SupplierPoDetailPage,
   },
 ];
